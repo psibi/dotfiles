39d8dc73d739f9b309cc5ca6ae8ad10daabd65e8
Sibi <sibi@psibi.in>
2013-08-08 16:10:45 +0530

    Modified some more key bindings.

--- init.el
+++ init.el
@@ -90,3 +90,8 @@
 ;; Rebind Meta key to C-x-m or C-c-m (more forgiving!)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
+
+(global-set-key "\C-w" 'backward-kill-word)
+(global-set-key "\C-x\C-k" 'kill-region)
+(global-set-key "\C-c\C-k" 'kill-region)
+
