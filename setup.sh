069121f744b38132f9f6523d824bda160a5f6574
Sibi <sibi@psibi.in>
2013-08-26 20:24:34 +0530

    Check to see if .emacs.d exists.

--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@
 ln -s .alias ~/.alias
 ln -s .global_ignore ~/.global_ignore
 
+if [ ! -d ~/.emacs.d ]; then
+    mkdir -v ~/.emacs.d
+fi
+
 cp -v init.el ~/.emacs.d/init.el
 
 git config --global core.excludefile ~/.global_ignore
