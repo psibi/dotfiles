64fc10c72e373d1b972153ee32ed764c9e5f3386
Sibi <sibi@psibi.in>
2014-01-22 16:39:05 +0530

    Utility functions.

--- init.el
+++ init.el
@@ -101,3 +101,34 @@
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
 (eval-after-load "which-func"
        '(add-to-list 'which-func-modes 'haskell-mode))
+
+;; ------------------------
+;; Useful utility functions
+;; ------------------------
+(defun rename-file-and-buffer ()
+  "Renames the current buffer and the file it's visiting"
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (if (get-buffer new-name)
+            (message "A buffer named '%s' already exists!" new-name)
+         (progn (rename-file name new-name 1)
+                 (rename-buffer new-name)
+                 (set-visited-file-name new-name)
+                 (set-buffer-modified-p nil)))))))
+
+(defun delete-file-and-buffer ()
+  "Removes file connected to current buffer and kills buffer"
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (ido-kill-buffer)
+      (when (yes-or-no-p "Are you sure you want to remove this file? ")
+        (delete-file filename)
+        (kill-buffer buffer)
+        (message "File '%s' successfully removed" filename)))))
