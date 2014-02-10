3d3451513f383fe2d043abf64a4fbf9245134f67
Arjun <arjun2594@gmail.com>
2014-02-10 14:48:12 +0530

    xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,49 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import qualified XMonad.StackSet as W
+import System.IO
+import Control.Monad
+
+myWorkspaces = ["main", "web", "chat", "dev", "media", "float"]
+
+-- Define the workspace an application has to go to
+myManageHook = composeAll . concat $
+    [
+          -- Applications that go to web
+      [ className =? b --> viewShift "web"      | b <- myClassWebShifts  ]
+         -- Applications that go to chat
+    , [ resource  =? c --> doF (W.shift "chat") | c <- myClassChatShifts ]
+    , [ resource  =? c --> doF (W.shift "dev") | c <- myClassDevShifts ]
+    , [ resource  =? c --> doF (W.shift "float") | c <- myClassFloatShifts ]
+    ]
+    where
+        viewShift = doF . liftM2 (.) W.greedyView W.shift
+        myClassWebShifts  = ["Firefox"]
+        myClassChatShifts = ["Pidgin" ]
+        myClassDevShifts = ["emacs"]
+        myClassFloatShifts = ["gimp"]
+
+main = do
+  xmproc <- spawnPipe "/bin/xmobar ~/.xmobarrc"
+  xmonad $ defaultConfig
+    {
+      manageHook = manageDocks <+> myManageHook
+                   <+> manageHook defaultConfig
+    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+    , modMask = mod4Mask     -- Rebind Mod to the Windows key
+    , workspaces = myWorkspaces
+    } `additionalKeys`
+    [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+    , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+    , ((0, xK_Print), spawn "scrot")
+    ]
+
+-- main = do
+--   xmonad $ defaultConfig
