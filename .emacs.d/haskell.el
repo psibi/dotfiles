fe0f6e1f0a027d36d3806e400c5935d423306c74
Sibi <sibi@psibi.in>
2014-06-12 10:29:48 +0530

    Hook defined.

--- .emacs.d/haskell.el
+++ .emacs.d/haskell.el
@@ -1,4 +1,4 @@
-;; hoogle, hasktags
+;; hoogle, hasktags, stylish haskell
 
 (custom-set-variables
   '(haskell-process-suggest-remove-import-lines t)
@@ -7,7 +7,17 @@
 
 ;; Make sure that you use latest cabal
 (custom-set-variables
-  '(haskell-process-type 'cabal-repl))
+ '(haskell-process-type 'cabal-repl))
+
+(add-hook 'haskell-cabal-mode-hook 'haskell-cabal-hook)
+;; Haskell
+(add-hook 'haskell-mode-hook 'turn-on-hi2)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
+
+;; Stylish haskell
+(custom-set-variables
+ '(haskell-stylish-on-save t))
 
 ;; Haskell-mode bindings
 (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
@@ -19,11 +29,12 @@
 (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
 (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
 
-;; Cabal-mode bindings
-(define-key haskell-cabal-mode-map (kbd "C-'") 'haskell-interactive-bring)
-(define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
-(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+;; Useful to have these keybindings for .cabal files, too.
+(defun haskell-cabal-hook ()
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-cabal-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch))
 
 ;; Make sure hasktags is installed
 (custom-set-variables
@@ -42,4 +53,4 @@
 ;; Some hooks
 (add-hook 'haskell-mode-hook 'turn-on-hi2)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)+ 3
+(add-hook 'haskell-mode-hook 'turn-on-haskell-decl-scan)
