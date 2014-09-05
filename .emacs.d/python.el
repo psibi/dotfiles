7e7d8008fce555c1e4c5a93401dd794f7e76816d
Sibi <sibi@psibi.in>
2014-09-06 01:30:08 +0530

    Some useful python utilities.

--- .emacs.d/python.el
+++ .emacs.d/python.el
@@ -1,6 +1,21 @@
 (add-hook 'python-mode-hook 'sibi-customization)
 
 (defun sibi-customization ()
-  (define-key python-mode-map (kbd "C-'") 'python-shell-switch-to-shell)
-  (define-key python-mode-map (kbd "C-c C-l") 'python-shell-send-buffer)
-  )
+  (define-key python-mode-map (kbd "C-'") 'sibi-python-shell)
+  (define-key python-mode-map (kbd "C-c C-l") 'sibi-python-load-buffer))
+
+(defun sibi-python-load-buffer ()
+  (interactive)
+  (progn
+    (setq cur-buf-name (buffer-name))
+    (python-shell-switch-to-shell)
+    (erase-buffer)
+    (switch-to-buffer-other-window cur-buf-name)
+    (python-shell-send-buffer)))
+
+(defun sibi-python-shell ()
+  (interactive)
+  (progn
+    (setq cur-buf-name (buffer-name))
+    (python-shell-switch-to-shell)
+    (switch-to-buffer-other-window cur-buf-name)))
