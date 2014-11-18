341304616aae77602bb201da81b2aa5c0cb86652
Sibi <sibi@stickystamp.com>
2014-11-18 16:44:34 +0530

    Re-organized.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -30,6 +30,8 @@
 
 (load-file "~/.emacs.d/haskell.el")
 (load-file "~/.emacs.d/python.el")
+(load-file "~/.emacs.d/web.el")
+(load-file "~/.emacs.d/sibi-utils.el")
 ;(load-file "~/.emacs.d/sml.el")
 
 (setq package-archives
@@ -110,37 +112,6 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
 (global-set-key (kbd "C-x m") 'shell)
-
-;; ------------------------
-;; Useful utility functions
-;; ------------------------
-(defun rename-file-and-buffer ()
-  "Renames the current buffer and the file it's visiting"
-  (interactive)
-  (let ((name (buffer-name))
-        (filename (buffer-file-name)))
-    (if (not filename)
-        (message "Buffer '%s' is not visiting a file!" name)
-      (let ((new-name (read-file-name "New name: " filename)))
-        (if (get-buffer new-name)
-            (message "A buffer named '%s' already exists!" new-name)
-         (progn (rename-file name new-name 1)
-                 (rename-buffer new-name)
-                 (set-visited-file-name new-name)
-                 (set-buffer-modified-p nil)))))))
-
-(defun delete-file-and-buffer ()
-  "Removes file connected to current buffer and kills buffer"
-  (interactive)
-  (let ((filename (buffer-file-name))
-        (buffer (current-buffer))
-        (name (buffer-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (ido-kill-buffer)
-      (when (yes-or-no-p "Are you sure you want to remove this file? ")
-        (delete-file filename)
-        (kill-buffer buffer)
-        (message "File '%s' successfully removed" filename)))))
 
 (smex-initialize)
 
