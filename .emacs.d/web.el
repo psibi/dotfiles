f40f3cfc8e98be7acd88cdd4056e1526c9316354
Sibi <sibi@stickystamp.com>
2014-12-03 20:53:32 +0530

    Added js2-mode.

--- .emacs.d/web.el
+++ .emacs.d/web.el
@@ -1,2 +1,7 @@
+(require 'js2-mode)
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
 (add-hook 'html-mode-hook 'turn-off-auto-fill)
 
