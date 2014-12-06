f55e98a80f0b880a3b9dd0acfdd565ce656d67b3
Sibi <sibi@psibi.in>
2014-12-07 00:32:45 +0530

    skewer mode

--- .emacs.d/web.el
+++ .emacs.d/web.el
@@ -1,7 +1,9 @@
 (require 'js2-mode)
+(require 'skewer-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (add-hook 'html-mode-hook 'turn-off-auto-fill)
+(add-hook 'js2-mode-hook 'skewer-mode)
 
