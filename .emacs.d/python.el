7cda3789f48d469f14ca7507a939e37fb74a34a1
Sibi <sibi@psibi.in>
2014-12-14 17:50:08 +0530

    Changed to ipython. Works perfectly now.

--- .emacs.d/python.el
+++ .emacs.d/python.el
@@ -1,3 +1,13 @@
+(require 'python) ;; from python.el (Don't use python-mode)
+
+(setq python-shell-interpreter "ipython")
+(setq python-shell-interpreter-args "--colors=Linux")
+(setq python-shell-prompt-regexp "In \\[[0-9]+\\]: ")
+(setq python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: ")
+(setq python-shell-completion-setup-code "from IPython.core.completerlib import module_completion")
+(setq python-shell-completion-module-string-code  "';'.join(module_completion('''%s'''))\n")
+(setq python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
 (add-hook 'python-mode-hook 'sibi-customization)
 
 (setenv "PYTHONPATH" ".:..")
