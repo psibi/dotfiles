42264505d3dce41771299461c24bb9fb718ed9e3
Sibi <sibi@stickystamp.com>
2014-12-22 22:41:03 +0530

    Custom split functions.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -185,3 +185,23 @@
 
 ;; Enable clipboard
 (setq x-select-enable-clipboard t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Custom splitting functions ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun vsplit-last-buffer ()
+  (interactive)
+  (split-window-vertically)
+  (other-window 1 nil)
+  (switch-to-next-buffer)
+  )
+(defun hsplit-last-buffer ()
+  (interactive)
+  (split-window-horizontally)
+  (other-window 1 nil)
+  (switch-to-next-buffer)
+  )
+
+(global-set-key (kbd "C-x 2") 'vsplit-last-buffer)
+(global-set-key (kbd "C-x 3") 'hsplit-last-buffer)
