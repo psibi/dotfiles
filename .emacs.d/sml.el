1acf76efcbb1c186cfcdf4e8488bd39337adcf7a
Sibi <sibi@psibi.in>
2015-02-28 00:37:25 +0530

    Port sml to use-package.

--- .emacs.d/sml.el
+++ .emacs.d/sml.el
@@ -1,6 +1,7 @@
-(require 'sml-mode)
+(use-package sml-mode
+  :config
+  (progn
+    (defun sibi-sml-customization ()
+      (define-key sml-mode-map (kbd "C-'") 'sml-prog-proc-switch-to))
 
-(add-hook 'sml-mode-hook 'sibi-sml-customization)
-
-(defun sibi-sml-customization ()
-  (define-key sml-mode-map (kbd "C-'") 'sml-prog-proc-switch-to))
+    (add-hook 'sml-mode-hook 'sibi-sml-customization)))
