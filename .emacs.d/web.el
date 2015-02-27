72ce3b5063cc286bdaf154a15f57d84f2a2a5626
Sibi <sibi@psibi.in>
2015-02-28 00:19:57 +0530

    web port to use-package.

--- .emacs.d/web.el
+++ .emacs.d/web.el
@@ -1,12 +1,19 @@
-(require 'js2-mode)
-(require 'skewer-mode)
+(use-package js2-mode
+  :ensure t
+  :mode "\\.js\\'"
+  :interpreter "node"
+  :config
+  (progn
+    (use-package skewer-mode
+      :ensure t
+      :init
+      (progn
+        (add-hook 'js2-mode-hook 'skewer-mode)))))
 
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-
-(add-hook 'html-mode-hook 'turn-off-auto-fill)
-(add-hook 'html-mode-hook 'skewer-mode)
-(add-hook 'js2-mode-hook 'skewer-mode)
-
-(define-key skewer-mode-map (kbd "C-'") 'skewer-repl)
-
+(use-package html-mode
+  :mode "\\.html\\'"
+  :bind ("C-'" . skewer-repl)
+  :config
+  (progn
+    (add-hook 'html-mode-hook 'turn-off-auto-fill)
+    (add-hook 'html-mode-hook 'skewer-mode)))
