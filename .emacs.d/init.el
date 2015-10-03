7034e175369dbf6ccc9fd47cea96c55024dca667
Sibi <sibi@psibi.in>
2015-10-03 19:35:08 +0530

    Modeline gets better with this.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -29,7 +29,10 @@
   :ensure t
   :init
   (progn
-    (load-theme 'spacemacs-dark t)))
+    (load-theme 'spacemacs-dark t)
+    (load-theme 'wheatgrass t)
+    ;; Exploits a bug to get a better modeline
+    ))
 
 (use-package server
   :ensure t
