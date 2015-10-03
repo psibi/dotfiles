f98e537f4a00c22927faf85c301b649aaae05035
Sibi <sibi@psibi.in>
2015-10-03 18:55:23 +0530

    Bootstrap use-package itself.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,6 +11,10 @@
 
 (package-initialize)
 
+;; Bootstrap `use-package`
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 (require 'use-package)
 
 (use-package cl)
