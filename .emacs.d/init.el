d2bd18046b5811c6a6213adb4c9ae6a2155a8fa6
Sibi Prabakaran <sibi@psibi.in>
2015-11-15 08:29:34 +0530

    Have only gnu and melpa repository.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,9 +4,6 @@
 
 (setq package-archives
       '(("gnu"         . "http://elpa.gnu.org/packages/")
-        ("original"    . "http://tromey.com/elpa/")
-        ("org"         . "http://orgmode.org/elpa/")
-        ("marmalade"   . "http://marmalade-repo.org/packages/")
         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (package-initialize)
