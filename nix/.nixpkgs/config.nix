60617a337864ebef56269f777ca26997d80aa3ee
Sibi Prabakaran <sibi@psibi.in>
2015-11-22 16:28:40 +0530

    Bump compiler version.

--- nix/.nixpkgs/config.nix
+++ nix/.nixpkgs/config.nix
@@ -2,9 +2,10 @@
 # Install like this: nix-env -iA nixpkgs.sibiHaskellEnv
 # Probably need to reduce the global dependencies ?
 {
+  allowUnfree = true; 
   packageOverrides = super: let self = super.pkgs; in
   {
-    sibiHaskellEnv = self.haskell.packages.ghc7101.ghcWithPackages
+    sibiHaskellEnv = self.haskell.packages.ghc7102.ghcWithPackages
                      (haskellPackages: with haskellPackages; [
                        xmonad-contrib
                        mtl
