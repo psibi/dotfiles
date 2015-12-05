953cda19bf0dc7c26c1eb222142d4858de03a039
Sibi Prabakaran <sibi@psibi.in>
2015-12-05 23:53:21 +0530

    Fix Auctex installation.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -97,18 +97,11 @@
 ;; -------------------
 ;; Everything in UTF-8
 ;; -------------------
-(prefer-coding-system                   'utf-8)
-(set-language-environment               'utf-8)
-(set-default-coding-systems             'utf-8)
-(setq file-name-coding-system           'utf-8)
-(setq default-buffer-file-coding-system 'utf-8)
-(setq coding-system-for-write           'utf-8)
-(set-keyboard-coding-system             'utf-8)
-(set-terminal-coding-system             'utf-8)
-(set-clipboard-coding-system            'utf-8)
-(set-selection-coding-system            'utf-8)
-(setq default-process-coding-system     '(utf-8 . utf-8))
-(add-to-list 'auto-coding-alist         '("." . utf-8))
+;; Not everything - as this stops the installation of auctex
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
