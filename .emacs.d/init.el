baede6b4eaf977bdbc57fc9fb4219121783b9754
Sibi Prabakaran <sibi@psibi.in>
2016-05-04 00:51:50 +0530

    Use expand-region and selected package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -396,6 +396,19 @@
   :init
   (mode-icons-mode))
 
+(use-package expand-region
+  :ensure t)
+
+(use-package selected
+  :ensure t
+  :init (selected-minor-mode)
+  :bind (:map selected-keymap
+              ("w" . er/expand-region)
+              ("q" . selected-off)
+              ("u" . upcase-region)
+              ("d" . downcase-region)
+              ("m" . apply-macro-to-region-lines)))
+
 ;; Isearch convenience, space matches anything (non-greedy)
 ;; Note that you can use universal argument
 (setq search-whitespace-regexp ".*?")
