a8d0027be71403428aae12a460cd7d5b3d592391
Sibi Prabakaran <sibi@psibi.in>
2016-08-29 01:16:42 +0530

    Add git user name and email as default config

--- setup.sh
+++ setup.sh
@@ -17,8 +17,11 @@ fi
 
 cp -v init.el ~/.emacs.d/init.el
 
+git config --global user.name "Sibi Prabakaran"
 git config --global core.excludefile ~/.global_ignore
 git config --global commit.gpgsign true
+git config --global user.email "sibi@psibi.in"
+
 
 ln ./.bashrc ~/.sibi_bashrc
 echo "source ~/.sibi_bashrc" >> ~/.bashrc
@@ -34,13 +37,19 @@ cp -v ./.Xresources ~/
 "echo Setting up GNU Screen configurations"
 cp -v ./.screenrc ~/
 
-echo "Setting bindings for Virtual Console"
-sudo ./virtual.sh
+# echo "Setting bindings for Virtual Console"
+# sudo ./virtual.sh
 
 echo "Copying ghci configuration"
 cp -v ./.ghci ~/
 chmod 700 ~/.ghci
 
-echo "\nDon't forget to put settings to /etc/rc.local \n
-      See virtual.sh for more details."
+# echo "\nDon't forget to put settings to /etc/rc.local \n
+#      See virtual.sh for more details."
+
+# https://nixmeal.wordpress.com/2012/07/24/copypaste-text-in-urxvt-rxvt-unicode-using-keyboard/
+echo "Setting up urxvt clipboard config"
+
+
+
 
