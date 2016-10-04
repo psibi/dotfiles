5d7fe9a8afff91f472e17ce8ce46c7801fff251a
Sibi Prabakaran <sibi@psibi.in>
2016-10-04 11:29:05 +0530

    have xmonad specific settingg

--- xmonad.sh
+++ xmonad.sh
@@ -2,6 +2,8 @@
 # Author: Sibi <sibi@psibi.in>
 # Note: Doesn't take care of installation of the related softwares.
 
+cp -v ./.Xresources ~/
+
 cp -r ./.xmonad ~/
 cp ./.xsession ~/
 cp ./.xmobarrc ~/
