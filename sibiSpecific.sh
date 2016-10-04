17c07a2c1baaefe218f1c12d5248582fb95bca3b
Sibi Prabakaran <sibi@psibi.in>
2016-10-04 11:28:50 +0530

    have my specific file

---
+++ sibiSpecific.sh
@@ -0,0 +1,6 @@
+git config --global user.name "Sibi Prabakaran"
+git config --global core.excludefile ~/.global_ignore
+git config --global commit.gpgsign true
+git config --global user.email "sibi@psibi.in"
+
+
