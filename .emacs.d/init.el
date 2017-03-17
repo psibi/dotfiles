522e41b4df0e48800b6dbd04606d5ffd0754b282
Sibi Prabakaran <sibi@psibi.in>
2017-03-17 12:22:20 +0530

    Add shift-region function

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -430,6 +430,38 @@
               ("g" . google-this)
               ("m" . apply-macro-to-region-lines)))
 
+(use-package markdown-mode
+  :ensure t
+  :mode "\\.md\\'")
+
+;; Shift the selected region right if distance is postive, left if
+;; negative
+
+(defun shift-region (distance)
+  (let ((mark (mark)))
+    (save-excursion
+      (indent-rigidly (region-beginning) (region-end) distance)
+      (push-mark mark t t)
+      ;; Tell the command loop not to deactivate the mark
+      ;; for transient mark mode
+      (setq deactivate-mark nil))))
+
+(defun shift-right ()
+  (interactive)
+  (shift-region 1))
+
+(defun shift-left ()
+  (interactive)
+  (shift-region -1))
+
+;; Bind (shift-right) and (shift-left) function to your favorite keys. I use
+;; the following so that Ctrl-Shift-Right Arrow moves selected text one 
+;; column to the right, Ctrl-Shift-Left Arrow moves selected text one
+;; column to the left:
+
+(global-set-key (kbd "C-c >") 'shift-right)
+(global-set-key (kbd "C-c <") 'shift-left)
+
 ;; Isearch convenience, space matches anything (non-greedy)
 ;; Note that you can use universal argument
 (setq search-whitespace-regexp ".*?")
