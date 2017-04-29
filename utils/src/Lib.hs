b89400fd1d8d6a24e0daa28a8d9126b7017a092e
Sibi Prabakaran <sibi@psibi.in>
2017-04-29 06:59:46 +0530

    Add more functionality

--- utils/src/Lib.hs
+++ utils/src/Lib.hs
@@ -14,6 +14,12 @@ module Lib
   , navigateParent
   , deployFish
   , syncFish
+  , syncEmacs
+  , deployEmacs
+  , initializeSystem
+  , syncXMonad
+  , deployXMonad
+  , deployBash
   ) where
 
 import Data.Conduit.Shell hiding (strip)
@@ -29,8 +35,17 @@ import Data.Text (Text, pack)
 import Data.Attoparsec.Text hiding (take)
 import Data.Word
 import Data.List (isSuffixOf, intersperse)
-import System.Directory (doesDirectoryExist, getHomeDirectory)
+import System.Directory
+       (doesDirectoryExist, getHomeDirectory, doesFileExist,
+        createDirectoryIfMissing)
 import System.FilePath
+import Data.Conduit (await)
+import Data.Conduit.Binary (sourceFile)
+import qualified Data.Conduit.Binary as C
+import Control.Monad.Trans.Resource
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as B8
+import Control.Monad.IO.Class (liftIO)
 
 releaseInfo :: IO ()
 releaseInfo = do
@@ -127,12 +142,13 @@ compress :: FilePath -> IO ()
 compress fname = run $ tar "-czvf" (fname <> ".tar.gz") fname
 
 navigateParent :: Int -> IO ()
-navigateParent level = if (level <= 2)
-                       then return ()
-                       else cd dir
-    where
-      dots = take (level - 1) $ repeat ".."
-      dir = concat $ intersperse "/" dots
+navigateParent level =
+  if (level <= 2)
+    then return ()
+    else cd dir
+  where
+    dots = take (level - 1) $ repeat ".."
+    dir = concat $ intersperse "/" dots
 
 dotfilesDir :: IO FilePath
 dotfilesDir = do
@@ -148,11 +164,12 @@ deployFish = do
       dirsDep = [dotDir </> ".config" </> "fish" </> "functions"]
       fishDir = hdir </> ".config" </> "fish"
   case exist of
-    True -> run $ do
-               mapM_ (\x -> cp "-v" x fishDir) filesDep
-               mapM_ (\x -> cp "-rv" x fishDir) dirsDep
+    True ->
+      run $
+      do mapM_ (\x -> cp "-v" x fishDir) filesDep
+         mapM_ (\x -> cp "-rv" x fishDir) dirsDep
     False -> putStrLn "Error: dotfiles in invalid location"
-  
+
 syncFish :: IO ()
 syncFish = do
   dotDir <- dotfilesDir
@@ -163,8 +180,109 @@ syncFish = do
       fishDir = hdir </> ".config" </> "fish"
       fishDotDir = dotDir </> ".config" </> "fish"
   case exists of
-    True -> run $ do
-                mapM_ (\x -> cp "-v" (fishDir </> x) fishDotDir) files
-                mapM_ (\x -> cp "-vr" (fishDir </> x) fishDotDir) dirs
+    True ->
+      run $
+      do mapM_ (\x -> cp "-v" (fishDir </> x) fishDotDir) files
+         mapM_ (\x -> cp "-vr" (fishDir </> x) fishDotDir) dirs
     False -> putStrLn "Error: dotfiles in invalid location"
-                
+
+removeFileIfExists :: FilePath -> IO ()
+removeFileIfExists fname = do
+  exist <- doesFileExist fname
+  case exist of
+    True -> run $ rm "-v" fname
+    False -> return ()
+
+removeDirectoryIfExists :: FilePath -> IO ()
+removeDirectoryIfExists fname = do
+  exist <- doesDirectoryExist fname
+  case exist of
+    True -> run $ rm "-vr" fname
+    False -> return ()
+
+initializeSystem :: IO ()
+initializeSystem = do
+  let files =
+        [ ".alias"
+        , ".global_ignore"
+        , ".screenrc"
+        , ".ghci"
+        , ".Xmodmap"
+        , ".Xresources"
+        , ".xmobarrc"
+        , ".xsession"
+        ]
+  hdir <- getHomeDirectory
+  dotDir <- dotfilesDir
+  mapM_ (\x -> removeFileIfExists (hdir </> x)) files
+  mapM_ (\x -> run $ cp "-v" (dotDir </> x) hdir) files
+
+deployEmacs :: IO ()
+deployEmacs = do
+  let files =
+        [ "haskell.el"
+        , "python.el"
+        , "web.el"
+        , "init.el"
+        , "sibi-utils.el"
+        , "sml.el"
+        ]
+  let emacsDir = ".emacs.d"
+  dotDir <- dotfilesDir
+  hdir <- getHomeDirectory
+  createDirectoryIfMissing False emacsDir
+  mapM_ (\x -> run $ cp "-v" (dotDir </> emacsDir </> x) (hdir </> emacsDir)) files
+
+syncEmacs :: IO ()
+syncEmacs = do
+  let files =
+        [ "haskell.el"
+        , "python.el"
+        , "web.el"
+        , "init.el"
+        , "sibi-utils.el"
+        , "sml.el"
+        ]
+  dotDir <- dotfilesDir
+  hdir <- getHomeDirectory
+  mapM_
+    (\x -> run $ cp "-v" (hdir </> ".emacs.d" </> x) (dotDir </> ".emacs.d"))
+    files
+
+deployXMonad :: IO ()
+deployXMonad = do
+  let xmonadDir = ".xmonad"
+  dotDir <- dotfilesDir
+  hdir <- getHomeDirectory
+  createDirectoryIfMissing False (hdir </> xmonadDir)
+  run $ cp "-v" (dotDir </> xmonadDir </> "*") (hdir </> xmonadDir)
+
+syncXMonad :: IO ()
+syncXMonad = do
+  let xmonadDir = ".xmonad"
+  dotDir <- dotfilesDir
+  hdir <- getHomeDirectory
+  run $ cp "-v" (hdir </> xmonadDir </> "*") (dotDir </> xmonadDir)
+
+contentInFile :: (MonadResource m) => Consumer BS.ByteString m Bool
+contentInFile = do
+  (bs :: Maybe BS.ByteString) <- await
+  let match = B8.pack "source ~/github/dotfiles/.bashrc"
+  case bs of
+    Nothing -> return False
+    Just bs' -> case (BS.isSuffixOf match bs') of
+                  True -> return True
+                  False -> contentInFile
+
+deployBash :: IO ()
+deployBash = do
+  dotDir <- dotfilesDir
+  hdir <- getHomeDirectory
+  let bashFile = ".bashrc"
+  val <- runConduitRes $ sourceFile (hdir </> bashFile) .| C.lines .| contentInFile
+  case val of
+    True -> return ()
+    False -> do
+             appendFile (hdir </> bashFile) "source ~/github/dotfiles/.bashrc"
+             putStrLn (bashFile <> " modified")
+
