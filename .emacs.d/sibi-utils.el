6aaed8e1b708b4a71e8514071cfcae6db5b9ccd6
Sibi Prabakaran <sibi@psibi.in>
2019-05-29 15:08:00 +0530

    Add another utility function

--- .emacs.d/sibi-utils.el
+++ .emacs.d/sibi-utils.el
@@ -72,13 +72,22 @@
         (replace-match "    "))
       )))
 
-;; Implementation taken from Batsov's prelude
-(defun sibi-copy-file-name-to-clipboard ()
+(defun sibi-copy-file-path-to-clipboard ()
   "Copy the current buffer file name to the clipboard."
   (interactive)
   (let ((filename (if (equal major-mode 'dired-mode)
                       default-directory
                     (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
+(defun sibi-copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-name))))
     (when filename
       (kill-new filename)
       (message "Copied buffer file name '%s' to the clipboard." filename))))
