c11591dcb014b8bcd579d2da972acc30fc1be986
Sibi Prabakaran <sibi@psibi.in>
2020-03-05 20:13:03 +0530

    Set variable

--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -5,10 +5,13 @@
 (setq org-agenda-files
 '("~/github/timebox/home.org" "~/github/timebox/learn.org" "~/github/timebox/oss.org"))
 
+;; (setq org-agenda-files (directory-files-recursively "~/github/timebox/" "\.org$"))
+
 (use-package org-journal
   :ensure t
   :init
   :custom
+  (org-journal-file-format "%Y-%m-%d.org")
   (org-journal-dir "~/github/timebox/2020/")
   (org-journal-date-format "%A, %d %B %Y")
   (org-journal-file-type 'daily))
