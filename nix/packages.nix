4375e5d63708b19766a20545dd99d10b0cd85ca5
Sibi Prabakaran <sibi@psibi.in>
2020-04-18 10:34:06 +0530

    Install full texlive

--- nix/packages.nix
+++ nix/packages.nix
@@ -6,4 +6,5 @@ with import ./nixpkgs.nix;
     xmobar gnucash fish emacs unrar pdftk kazam vlc ffmpeg bat fd nixfmt git
     cabal2nix keepassxc google-chrome slack jl rustup hlint htop;
   hindent = haskellPackages.hindent;
+  latex = texlive.combined.scheme-full;
 }
