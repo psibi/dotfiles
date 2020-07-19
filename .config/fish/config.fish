7a0cb8bbf7a36ce28d1632a1bfdd49230d613dff
Sibi Prabakaran <sibi@psibi.in>
2020-07-19 11:45:22 +0530

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,3 +6,6 @@ set -g --export GPG_TTY (tty)
 set fish_greeting
 
 set PATH /home/sibi/bin /home/sibi/.cargo/bin $PATH
+
+# https://github.com/fish-shell/fish-shell/issues/825
+alias hr 'history --merge'  # read and merge history from disk
--- nix/packages.nix
+++ nix/packages.nix
@@ -3,11 +3,14 @@ with import ./nixpkgs.nix;
   inherit ripgrep
     # xscreensaver
     # alacritty;
+    # google-chrome
+    # slack 
     sibi-xmonad rxvt_unicode-with-plugins feh screen xclip xsel xdotool eboard
-    gnucash fish emacs unrar pdftk kazam vlc ffmpeg bat fd nixfmt git
-    cabal2nix keepassxc google-chrome slack jl rustup hlint htop powertop font-awesome symbola sibi-xmobar;
+    gnucash fish emacs unrar pdftk kazam vlc ffmpeg bat fd nixfmt git direnv
+    cabal2nix keepassxc jl rustup hlint htop powertop font-awesome symbola sibi-xmobar;
   hindent = haskellPackages.hindent;
   pandoc = haskellPackages.pandoc;
   latex = texlive.combined.scheme-full;
+  # latex = texlive.combined.scheme-basic;
   sibi-utils = haskellPackages.callPackage ../utils/sibi-utils.nix {};
 }
