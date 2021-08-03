c11f4d457784f01fda6b70476db33b7513b0be41
Sibi Prabakaran <sibi@psibi.in>
2021-08-03 17:05:40 +0530

    Fix ouch package

--- machines/packages/ouch/default.nix
+++ machines/packages/ouch/default.nix
@@ -1,4 +1,4 @@
-{ lib, rustPlatform, fetchFromGitHub }:
+{ lib, rustPlatform, fetchFromGitHub, fetchurl }:
 
 rustPlatform.buildRustPackage rec {
   pname = "ouch";
@@ -11,14 +11,11 @@ rustPlatform.buildRustPackage rec {
     sha256 = "00ah8hgrppa61jhwb74zl5b509q0yp2pp27w9frm814iqx70qn38";
   };
 
-  # cargoPatches = [
-  #   # a patch file to add/update Cargo.lock in the source code
-  #   ./add-Cargo.lock.patch
-  # ];
-
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-  };
+  cargoPatches = [
+    # a patch file to add Cargo.lock in the source code
+    # https://github.com/vrmiguel/ouch/pull/46
+    ./add-Cargo.lock.patch
+  ];
 
   cargoSha256 = "181aq8r78g4bl1ndlwl54ws5ccrwph0mmk9506djxvfdy3hndxkg";
 
