34e8b159c4e70759ae6764da630e410325c4f73e
Sibi Prabakaran <sibi@psibi.in>
2021-08-10 15:34:03 +0530

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,7 @@ set PATH /home/sibi/bin /home/sibi/.cargo/bin $PATH
 
 # https://github.com/fish-shell/fish-shell/issues/825
 alias hr 'history --merge'  # read and merge history from disk
+
+starship init fish | source
+direnv hook fish | source
+zoxide init fish | source
