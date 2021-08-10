8c000a5b431a9aa184417398113818382927973f
Sibi Prabakaran <sibi@psibi.in>
2021-08-10 15:34:50 +0530

    Cleanup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,9 @@
 source ~/github/dotfiles/.alias
 
-set -g --export GPG_TTY (tty)
-
 # Disable greeting
 set fish_greeting
 
-set PATH /home/sibi/bin /home/sibi/.cargo/bin $PATH
+# set PATH /home/sibi/.cargo/bin $PATH
 
 # https://github.com/fish-shell/fish-shell/issues/825
 alias hr 'history --merge'  # read and merge history from disk
