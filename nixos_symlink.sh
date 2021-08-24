1e85d10847f14edc1b18f717f5d9d86dc2e3a939
Sibi Prabakaran <sibi@psibi.in>
2021-08-24 21:43:32 +0530

    Add nixos symlink

---
+++ nixos_symlink.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+ln -v -s $PWD/bin/em $HOME/bin/em
+
+ln -v -s $PWD/.screenrc $HOME/.screenrc
+
+ln -v -s $PWD/.gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
+
+ln -v -s $PWD/.config/fish/config.fish $HOME/.config/fish/config.fish
+
+ln -v -s $PWD/alacritty.yml $HOME/.alacritty.yml
