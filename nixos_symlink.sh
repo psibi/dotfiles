2a0e2dde132639cbcd7df7a55aceb73e77647e19
Sibi Prabakaran <sibi@psibi.in>
2021-08-26 11:26:51 +0530

    Add starship configuration

--- nixos_symlink.sh
+++ nixos_symlink.sh
@@ -9,3 +9,5 @@ ln -v -s $PWD/.gnupg/gpg-agent.conf $HOME/.gnupg/gpg-agent.conf
 ln -v -s $PWD/.config/fish/config.fish $HOME/.config/fish/config.fish
 
 ln -v -s $PWD/alacritty.yml $HOME/.alacritty.yml
+
+ln -v -s $PWD/.config/starship.toml $HOME/.config/starship.toml
