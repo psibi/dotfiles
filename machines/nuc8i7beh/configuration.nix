952c9cc60a82cdede7e1ede15d09dd4691f613b3
Sibi Prabakaran <sibi@psibi.in>
2022-07-27 12:39:44 +0530

    Enable dconf

--- machines/nuc8i7beh/configuration.nix
+++ machines/nuc8i7beh/configuration.nix
@@ -141,6 +141,10 @@
 
   environment.homeBinInPath = true;
 
+  # This makes gnucash preferences work for a specific user
+  # More details here: https://github.com/NixOS/nixpkgs/issues/168205
+  programs.dconf.enable = true;
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
