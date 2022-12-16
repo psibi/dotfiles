15ea7e1131e94dea907e4994751e47d225984348
Sibi Prabakaran <sibi@psibi.in>
2022-12-16 20:03:09 +0530

    Cleanup sibi-utils

--- .emacs.d/sibi-utils.el
+++ .emacs.d/sibi-utils.el
@@ -29,48 +29,12 @@
         (kill-buffer buffer)
         (message "File '%s' successfully removed" filename)))))
 
-(defun latex-clean-table (start end)
-  "Clean latex table combined with Math elements"
-  (interactive "r")
-  (save-restriction
-    (narrow-to-region start end)
-    (goto-char (point-min))
-    (while (search-forward "|l" nil t) (replace-match "| >{$}l<{$} " nil t))
-    (goto-char (point-min))
-    (while (search-forward "\$\\backslash\$" nil t) (replace-match "" nil t))
-    (goto-char (point-min))
-    (while (search-forward "$<$" nil t) (replace-match "<" nil t))
-    (goto-char (point-min))
-    (while (search-forward "$>$" nil t) (replace-match "<" nil t))
-    (goto-char (point-min))
-    (while (search-forward "implies" nil t) (replace-match "\implies" nil t))
-    (goto-char (point-min))
-    (while (search-forward "\\^" nil t) (replace-match "^" nil t))
-    ))
-
 ;;; It is the opposite of fill-paragraph  (Credits to Stefan Monnier)
 (defun unfill-paragraph ()
   "Takes a multi-line paragraph and makes it into a single line of text."
   (interactive)
   (let ((fill-column (point-max)))
     (fill-paragraph nil)))
-
-;; For github issues
-(defun sibi-four-space-indent (beginning end)
-  (interactive "r")
-  (if (use-region-p)
-      (save-restriction
-        (narrow-to-region beginning end)
-        (save-excursion
-          (goto-char (point-min))
-          (while (re-search-forward "^" nil t)
-            (replace-match "    "))
-          ))
-    (save-excursion
-      (goto-char (point-min))
-      (while (re-search-forward "^" nil t)
-        (replace-match "    "))
-      )))
 
 (defun sibi-copy-file-path-to-clipboard ()
   "Copy the current buffer file name to the clipboard."
@@ -91,31 +55,6 @@
     (when filename
       (kill-new filename)
       (message "Copied buffer file name '%s' to the clipboard." filename))))
-
-(defun toggle-window-split ()
-  (interactive)
-  (if (= (count-windows) 2)
-      (let* ((this-win-buffer (window-buffer))
-             (next-win-buffer (window-buffer (next-window)))
-             (this-win-edges (window-edges (selected-window)))
-             (next-win-edges (window-edges (next-window)))
-             (this-win-2nd (not (and (<= (car this-win-edges)
-                                         (car next-win-edges))
-                                     (<= (cadr this-win-edges)
-                                         (cadr next-win-edges)))))
-             (splitter
-              (if (= (car this-win-edges)
-                     (car (window-edges (next-window))))
-                  'split-window-horizontally
-                'split-window-vertically)))
-        (delete-other-windows)
-        (let ((first-win (selected-window)))
-          (funcall splitter)
-          (if this-win-2nd (other-window 1))
-          (set-window-buffer (selected-window) this-win-buffer)
-          (set-window-buffer (next-window) next-win-buffer)
-          (select-window first-win)
-          (if this-win-2nd (other-window 1))))))
 
 (defun kill-other-buffers ()
     "Kill all other buffers."
