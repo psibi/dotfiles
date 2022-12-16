07b57a5989d69083d75402e8e001d866513d1538
Sibi Prabakaran <sibi@psibi.in>
2022-12-16 20:02:51 +0530

    Deleted python, sml and web.el

--- .emacs.d/sml.el
+++
@@ -1,7 +0,0 @@
-(use-package sml-mode
-  :config
-  (progn
-    (defun sibi-sml-customization ()
-      (define-key sml-mode-map (kbd "C-'") 'sml-prog-proc-switch-to))
-
-    (add-hook 'sml-mode-hook 'sibi-sml-customization)))
