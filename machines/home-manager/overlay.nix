553718f7ce56cd5f5cbcb5aa1f4aa783d6a11918
Sibi Prabakaran <sibi@psibi.in>
2023-10-29 10:39:28 +0530

    Add flarectl overlay

--- machines/home-manager/overlay.nix
+++ machines/home-manager/overlay.nix
@@ -1,4 +1,5 @@
 self: super: {
   em = import ./scripts/em.nix { pkgs = super.pkgs; };
   sibiEmacs = super.pkgs.callPackage ../packages/emacs/default.nix { pkgs = super.pkgs; };
+  flarectl = super.pkgs.callPackage ../packages/flarectl/default.nix { };
 }
