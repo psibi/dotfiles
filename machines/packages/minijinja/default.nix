a44d10b5b616615e7b4b0ffcf5d0e6f2277405e0
Sibi Prabakaran <sibi@psibi.in>
2024-02-05 08:14:41 +0530

    Update minijinja package

--- machines/packages/minijinja/default.nix
+++ machines/packages/minijinja/default.nix
@@ -2,21 +2,27 @@
 
 rustPlatform.buildRustPackage rec {
   pname = "minijinja";
-  version = "1.0.10";
+  version = "1.0.12";
 
   src = fetchFromGitHub {
     owner = "mitsuhiko";
     repo = pname;
     rev = version;
-    hash = "sha256-2OujlzRmHin6R9Dujo8wzVUI2IK5H2zyas9cfyQfyOA=";
+    hash = "sha256-v5YTPcUiCUQvTURqgKepdOjKZ5rFLr+mF7X+s5GvxdM=";
   };
 
-  cargoSha256 = lib.fakeHash;
+  cargoSha256 = "sha256-OhfrlT2DZU3ahH9PKqpEK8f34J2E6zNUai3hYBPg7v4=";
+
+  # https://github.com/mitsuhiko/minijinja/issues/402
+  doCheck = false;
+
+  cargoBuildFlags = "--bin minijinja-cli";
 
   meta = with lib; {
-    description = "Yet another language server for Nix";
-    homepage = "https://github.com/oxalica/nil";
-    license = with licenses; [ mit asl20 ];
+    description = "Command Line Utility to render MiniJinja/Jinja2 templates";
+    homepage = "https://github.com/mitsuhiko/minijinja";
+    license = with licenses; [ asl20 ];
     maintainers = with maintainers; [ psibi ];
+    mainProgram = "minijinja-cli";
   };
 }
