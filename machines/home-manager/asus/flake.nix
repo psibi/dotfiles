c972191d8589a2655be26a3c3cf2d1f0e56d1214
Sibi Prabakaran <sibi@psibi.in>
2024-05-09 21:33:36 +0530

    Update flake

--- machines/home-manager/asus/flake.nix
+++ machines/home-manager/asus/flake.nix
@@ -22,7 +22,7 @@
       };
     in
     {
-      homeConfigurations.arya = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations.hask = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         modules = [
           ./home.nix
