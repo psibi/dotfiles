2c909f48c06a4a1e05567c08e84433e6cdf7179b
Sibi Prabakaran <sibi@psibi.in>
2024-06-01 09:44:14 +0530

    Update flake

--- machines/latitude-7490/flake.nix
+++ machines/latitude-7490/flake.nix
@@ -2,7 +2,7 @@
   description = "Latitude 7490 System Setup";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-24.05";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
 
