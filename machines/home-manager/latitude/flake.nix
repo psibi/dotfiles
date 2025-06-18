d7679de1c270195b058fe50c0bdf8ee5a822e09b
Sibi Prabakaran <sibi@psibi.in>
2025-06-18 20:50:01 +0530

    Update lock files

--- machines/home-manager/latitude/flake.nix
+++ machines/home-manager/latitude/flake.nix
@@ -2,10 +2,10 @@
   description = "Latitude 7490 System Setup";
 
   inputs = {
-    nixpkgs = { url = "nixpkgs/nixos-24.11"; };
+    nixpkgs = { url = "nixpkgs/nixos-25.05"; };
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
