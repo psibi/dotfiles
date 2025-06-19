fa13453f9536c438b6094fc8da040d6db9c1ebb6
Sibi Prabakaran <sibi@psibi.in>
2025-06-19 13:37:22 +0530

    Update overlay

--- machines/home-manager/overlay.nix
+++ machines/home-manager/overlay.nix
@@ -2,6 +2,7 @@ self: super: {
   em = import ./scripts/em.nix { pkgs = super.pkgs; };
   sibiEmacs = super.pkgs.callPackage ../packages/emacs/default.nix { pkgs = super.pkgs; };
   flarectl = super.pkgs.callPackage ../packages/flarectl/default.nix { };
+  tombi = super.pkgs.callPackage ../packages/tombi/default.nix {};
   # minijinja-cli = super.pkgs.callPackage ../packages/minijinja/default.nix { };
   sibi-yaml-language-server = super.pkgs.callPackage ../packages/yaml-language-server/default.nix {};
   flameshot-grim = super.pkgs.flameshot.overrideAttrs (oldAttrs: {
