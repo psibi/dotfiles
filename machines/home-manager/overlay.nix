8e5d77ab8adc240c79e8cae234aa764df381c144
Sibi Prabakaran <sibi@psibi.in>
2025-12-06 16:16:57 +0530

    Comment out spl-token-cli

--- machines/home-manager/overlay.nix
+++ machines/home-manager/overlay.nix
@@ -3,7 +3,7 @@ self: super: {
   sibiEmacs = super.pkgs.callPackage ../packages/emacs/default.nix { pkgs = super.pkgs; };
   # flarectl = super.pkgs.callPackage ../packages/flarectl/default.nix { };
   # tombi = super.pkgs.callPackage ../packages/tombi/default.nix {};
-  spl-token-cli = super.pkgs.callPackage ../packages/spl-token-cli/default.nix {};
+  # spl-token-cli = super.pkgs.callPackage ../packages/spl-token-cli/default.nix {};
   # gemini-cli = super.pkgs.callPackage ../packages/gemini-cli/default.nix {};
   # minijinja-cli = super.pkgs.callPackage ../packages/minijinja/default.nix { };
   sibi-yaml-language-server = super.pkgs.callPackage ../packages/yaml-language-server/default.nix {};
